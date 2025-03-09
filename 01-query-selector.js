9759f49028dbc927e0f32efc71a8834004e329ff
Anna-1987 <mag260910@gmail.com>
2025-03-09 14:52:19 +0200

    classes

--- 01-query-selector.js
+++ 01-query-selector.js
@@ -1,12 +1,33 @@
 const magicBtn = document.querySelector('.js-magic-btn');
 
-magicBtn.addEventListener('click', () => {
-    console.log('покажи кнопку', magicBtn);
-});
+// magicBtn.addEventListener('click', () => {
+//     console.log('покажи кнопку', magicBtn);
+// });
 
 
 // const navEl = document.querySelector('ul');
 // console.log(navEl);
 
+
+magicBtn.addEventListener('click', () => {
+   const navEl = document.querySelector('.site_nav');
+console.log(navEl.classList);
+    navEl.classList.add('super-cool');//добавит класс
+    // navEl.classList.remove('super-cool');// удалит класс
+    navEl.classList.replace('super-cool','new-super-cool');// заменит старый класс на новый
+});
+
 const navEl = document.querySelector('.site_nav');
 console.log(navEl.classList);
+navEl.classList.add('super-cool');
+console.log(navEl.classList.contains('qwqwqw'))//проверяет есть ли такой класс на элементе
+
+// magicBtn.addEventListener('click', () => {
+//     navEl.classList.toggle('new-class');
+// })
+
+const currentPageUrl = '/portfolio';
+const linkEl = document.querySelector(`.site-nav_link[href="${currentPageUrl}"]`);
+
+console.log(linkEl);
+linkEl.classList.add('site-nav_color');
